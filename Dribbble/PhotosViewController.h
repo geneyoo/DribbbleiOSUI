ca5de4d58ff80a02e63230ef09c0db6b87ed471f
geneyoo <geneyoo@berkeley.edu>
2015-10-08 16:41:28 -0700

    initial commit

---
+++ Dribbble/PhotosViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dribbble
+//
+//  Created by Gene Yoo on 10/8/15.
+//  Copyright © 2015 gyoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotosViewController : UIViewController
+
+
+@end
+
---
+++ Dribbble/PhotosViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Dribbble
+//
+//  Created by Gene Yoo on 10/8/15.
+//  Copyright © 2015 gyoo. All rights reserved.
+//
+
+#import "PhotosViewController.h"
+
+@interface PhotosViewController ()
+
+@end
+
+@implementation PhotosViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
